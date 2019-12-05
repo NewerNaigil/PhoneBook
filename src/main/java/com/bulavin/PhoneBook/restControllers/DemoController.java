61f18a0e1fa6079ce0c4a6772b6d6807cd972797
NewerNaigil <newernaigil@yandex.ru>
2019-12-05 18:35:48 +0300

    Message

--- src/main/java/com/bulavin/PhoneBook/restControllers/DemoController.java
+++ src/main/java/com/bulavin/PhoneBook/restControllers/DemoController.java
@@ -31,32 +31,4 @@ public class DemoController {
     }
 
 
-
-
-
-
-
-
-//    public static class RestResp {
-//        String param1;
-//        String param2;
-//
-//        public String getParam1() {return param1;}
-//
-//        public void setParam1(String param1) {this.param1 = param1;}
-//
-//        public String getParam2() {return param2;}
-//
-//        public void setParam2(String param2) {this.param2 = param2;}
-//    }
-
-
-
-//    @RequestMapping(value = "/index", method = RequestMethod.GET)
-//    public RestResp restMethod(String name){
-//        RestResp result = new RestResp();
-//        result.setParam1("1");
-//        result.setParam2(name);
-//        return result;
-
     }
