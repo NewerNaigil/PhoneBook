b7a63b4cec585ba7f076999e2e3ad0102df6b7eb
NewerNaigil <newernaigil@yandex.ru>
2019-12-18 18:29:40 +0300

    Message

--- src/test/java/com/bulavin/PhoneBook/PhoneBookApplicationTests.java
+++ src/test/java/com/bulavin/PhoneBook/PhoneBookApplicationTests.java
@@ -1,13 +1,25 @@
 package com.bulavin.PhoneBook;
 
+import com.bulavin.PhoneBook.restControllers.Controller;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PhoneBookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private Controller controller;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
